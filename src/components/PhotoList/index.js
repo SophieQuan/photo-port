82c64f126fa83516965d9e9b2a9b45a9d36350a5
SophieQuan <sophiequan7@gmail.com>
2022-05-14 15:16:26 -0400

    Manage the Modal's state

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
-
+import Modal from "../Modal";
 const PhotoList = ({ category }) => {
+  //set the initial state of isModalOpen to false, because we don't want the modal to open until a user has clicked on an image.å
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
+
   const [photos] = useState([
     {
       name: "Grocery aisle",
@@ -119,15 +123,20 @@ const PhotoList = ({ category }) => {
   ]);
 
   const currentPhotos = photos.filter(photo => photo.category === category);
-
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(true);
+  };
   return (
     <div>
+      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
             src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
